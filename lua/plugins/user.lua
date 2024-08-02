e3a2c72fe348291b7b774d735733f6a1e7984e9e
Genesis Guerrero <genesis.guerrero@venturatravel.org>
2024-08-02 12:26:06 +0200

    toggle term

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -6,110 +6,111 @@
 ---@type LazySpec
 return {
 
-    -- customize alpha options
-    {
-        "goolord/alpha-nvim",
-        opts = function(_, opts)
-            -- customize the dashboard header
-            opts.section.header.val = {
-                "",
-                "    ▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄ ▄▄    ▄ ▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄ ▄▄▄ ▄▄▄▄▄▄▄",
-                "   █       █       █  █  █ █       █       █   █       █",
-                "   █   ▄▄▄▄█    ▄▄▄█   █▄█ █    ▄▄▄█  ▄▄▄▄▄█   █  ▄▄▄▄▄█",
-                "   █  █  ▄▄█   █▄▄▄█       █   █▄▄▄█ █▄▄▄▄▄█   █ █▄▄▄▄▄",
-                "   █  █ █  █    ▄▄▄█  ▄    █    ▄▄▄█▄▄▄▄▄  █   █▄▄▄▄▄  █",
-                "   █  █▄▄█ █   █▄▄▄█ █ █   █   █▄▄▄ ▄▄▄▄▄█ █   █▄▄▄▄▄█ █",
-                "   █▄▄▄▄▄▄▄█▄▄▄▄▄▄▄█▄█  █▄▄█▄▄▄▄▄▄▄█▄▄▄▄▄▄▄█▄▄▄█▄▄▄▄▄▄▄█",
-                "",
-                "            ███    ██ ██    ██ ██ ███    ███",
-                "            ████   ██ ██    ██ ██ ████  ████",
-                "            ██ ██  ██ ██    ██ ██ ██ ████ ██",
-                "            ██  ██ ██  ██  ██  ██ ██  ██  ██",
-                "            ██   ████   ████   ██ ██      ██",
-            }
-            return opts
-        end,
-    },
+  -- customize alpha options
+  {
+    "goolord/alpha-nvim",
+    opts = function(_, opts)
+      -- customize the dashboard header
+      opts.section.header.val = {
+        "",
+        "    ▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄ ▄▄    ▄ ▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄ ▄▄▄ ▄▄▄▄▄▄▄",
+        "   █       █       █  █  █ █       █       █   █       █",
+        "   █   ▄▄▄▄█    ▄▄▄█   █▄█ █    ▄▄▄█  ▄▄▄▄▄█   █  ▄▄▄▄▄█",
+        "   █  █  ▄▄█   █▄▄▄█       █   █▄▄▄█ █▄▄▄▄▄█   █ █▄▄▄▄▄",
+        "   █  █ █  █    ▄▄▄█  ▄    █    ▄▄▄█▄▄▄▄▄  █   █▄▄▄▄▄  █",
+        "   █  █▄▄█ █   █▄▄▄█ █ █   █   █▄▄▄ ▄▄▄▄▄█ █   █▄▄▄▄▄█ █",
+        "   █▄▄▄▄▄▄▄█▄▄▄▄▄▄▄█▄█  █▄▄█▄▄▄▄▄▄▄█▄▄▄▄▄▄▄█▄▄▄█▄▄▄▄▄▄▄█",
+        "",
+        "            ███    ██ ██    ██ ██ ███    ███",
+        "            ████   ██ ██    ██ ██ ████  ████",
+        "            ██ ██  ██ ██    ██ ██ ██ ████ ██",
+        "            ██  ██ ██  ██  ██  ██ ██  ██  ██",
+        "            ██   ████   ████   ██ ██      ██",
+      }
+      return opts
+    end,
+  },
 
-    "andweeb/presence.nvim",
-    {
-        "ray-x/lsp_signature.nvim",
-        event = "BufRead",
-        config = function() require("lsp_signature").setup() end,
-    },
+  "andweeb/presence.nvim",
+  {
+    "ray-x/lsp_signature.nvim",
+    event = "BufRead",
+    config = function() require("lsp_signature").setup() end,
+  },
 
-    {
-        "tpope/vim-fugitive",
-        lazy = false,
-    },
+  {
+    "tpope/vim-fugitive",
+    lazy = false,
+  },
 
-    { "phha/zenburn.nvim" },
+  { "phha/zenburn.nvim" },
 
-    -- You can disable default plugins as follows:
-    --{ "max397574/better-escape.nvim", enabled = false },
+  -- You can disable default plugins as follows:
+  --{ "max397574/better-escape.nvim", enabled = false },
 
-    -- You can also easily customize additional setup of plugins that is outside of the plugin's setup call
-    {
-        "L3MON4D3/LuaSnip",
-        config = function(plugin, opts)
-            require "astronvim.plugins.configs.luasnip" (plugin, opts) -- include the default astronvim config that calls the setup call
-            -- add more custom luasnip configuration such as filetype extend or custom snippets
-            local luasnip = require "luasnip"
-            luasnip.filetype_extend("javascript", { "javascriptreact" })
-        end,
-    },
+  -- You can also easily customize additional setup of plugins that is outside of the plugin's setup call
+  {
+    "L3MON4D3/LuaSnip",
+    config = function(plugin, opts)
+      require "astronvim.plugins.configs.luasnip"(plugin, opts) -- include the default astronvim config that calls the setup call
+      -- add more custom luasnip configuration such as filetype extend or custom snippets
+      local luasnip = require "luasnip"
+      luasnip.filetype_extend("javascript", { "javascriptreact" })
+    end,
+  },
 
-    {
-        "windwp/nvim-autopairs",
-        config = function(plugin, opts)
-            require "astronvim.plugins.configs.nvim-autopairs" (plugin, opts) -- include the default astronvim config that calls the setup call
-            -- add more custom autopairs configuration such as custom rules
-            local npairs = require "nvim-autopairs"
-            local Rule = require "nvim-autopairs.rule"
-            local cond = require "nvim-autopairs.conds"
-            npairs.add_rules(
-                {
-                    Rule("$", "$", { "tex", "latex" })
-                    -- don't add a pair if the next character is %
-                        :with_pair(cond.not_after_regex "%%")
-                    -- don't add a pair if  the previous character is xxx
-                        :with_pair(
-                            cond.not_before_regex("xxx", 3)
-                        )
-                    -- don't move right when repeat character
-                        :with_move(cond.none())
-                    -- don't delete if the next character is xx
-                        :with_del(cond.not_after_regex "xx")
-                    -- disable adding a newline when you press <cr>
-                        :with_cr(cond.none()),
-                },
-                -- disable for .vim files, but it work for another filetypes
-                Rule("a", "a", "-vim")
+  {
+    "windwp/nvim-autopairs",
+    config = function(plugin, opts)
+      require "astronvim.plugins.configs.nvim-autopairs"(plugin, opts) -- include the default astronvim config that calls the setup call
+      -- add more custom autopairs configuration such as custom rules
+      local npairs = require "nvim-autopairs"
+      local Rule = require "nvim-autopairs.rule"
+      local cond = require "nvim-autopairs.conds"
+      npairs.add_rules(
+        {
+          Rule("$", "$", { "tex", "latex" })
+            -- don't add a pair if the next character is %
+            :with_pair(cond.not_after_regex "%%")
+            -- don't add a pair if  the previous character is xxx
+            :with_pair(
+              cond.not_before_regex("xxx", 3)
             )
-        end,
-    },
-    {
-        "mg979/vim-visual-multi",
-        event = "User AstroGitFile",
-    },
-    {
-        "fatih/vim-go",
-        lazy = false,
-    },
-    {
-        "joerdav/templ.vim",
-    },
-    {
-        "akinsho/toggleterm.nvim",
-        lazy = false,
-        opts = {
-            open_mapping = [[<c-\>]],
+            -- don't move right when repeat character
+            :with_move(cond.none())
+            -- don't delete if the next character is xx
+            :with_del(cond.not_after_regex "xx")
+            -- disable adding a newline when you press <cr>
+            :with_cr(cond.none()),
         },
+        -- disable for .vim files, but it work for another filetypes
+        Rule("a", "a", "-vim")
+      )
+    end,
+  },
+  {
+    "mg979/vim-visual-multi",
+    event = "User AstroGitFile",
+  },
+  {
+    "fatih/vim-go",
+    lazy = false,
+  },
+  {
+    "joerdav/templ.vim",
+  },
+  {
+    "akinsho/toggleterm.nvim",
+    lazy = false,
+    opts = {
+      open_mapping = [[<c-\>]],
     },
-    {
-        "rcarriga/nvim-notify",
-        opts = {
-            background_colour = "#000000",
-        },
+  },
+  {
+    "rcarriga/nvim-notify",
+    opts = {
+      background_colour = "#000000",
+      direction = "float",
     },
+  },
 }
