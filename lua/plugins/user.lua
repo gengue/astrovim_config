86a432d2abe5cd75f65b2392c4b4c6a64afd47d1
Genesis Guerrero <genesisdaft@gmail.com>
2025-01-06 20:18:06 +0100

    new style

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -130,4 +130,18 @@ return {
     },
     config = true,
   },
+  {
+    "f-person/auto-dark-mode.nvim",
+    opts = {
+      update_interval = 1000,
+      set_dark_mode = function()
+        vim.api.nvim_set_option_value("background", "dark", {})
+        vim.cmd "colorscheme terafox"
+      end,
+      set_light_mode = function()
+        vim.api.nvim_set_option_value("background", "light", {})
+        vim.cmd "colorscheme astrolight"
+      end,
+    },
+  },
 }
