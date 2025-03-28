dc77b5e176159e87843663042a4f40a2a2fc5310
Genesis Guerrero <genesisdaft@gmail.com>
2025-03-28 14:57:03 +0100

    vesper is cool

--- lua/plugins/astroui.lua
+++ lua/plugins/astroui.lua
@@ -13,13 +13,15 @@ return {
     -- change colorscheme
     -- colorscheme = "catppuccin-mocha",
     -- colorscheme = "gruvbox",
-    colorscheme = "terafox",
+    -- colorscheme = "terafox",
+    colorscheme = "vesper",
+    -- colorscheme = "base16-black-metal-bathory",
     -- colorscheme = "rose-pine-moon",
     -- colorscheme = "duskfox",
 
     -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
     highlights = {
-      init = {
+      terafox = {
         Normal = { bg = "none" },
         NormalNC = { bg = "none" },
         NormalFloat = { bg = "none" },
@@ -30,6 +32,15 @@ return {
         -- Only neo-tree gets transparency
         NeoTreeNormal = { bg = "none" },
         NeoTreeNormalNC = { bg = "none" },
+        -- CursorLine = { bg = "#0f1c1e" },
+        CursorLine = { bg = "none" },
+      },
+      vesper = {
+        CursorLine = { bg = "none" },
+        Visual = { bg = "#33281f" },
+        Whitespace = { fg = "#19140f" },
+        Search = { bg = "#575757" },
+        IncSearch = { bg = "#575757" },
       },
       -- init = {
       --   Normal = { bg = "none" },
@@ -41,77 +52,6 @@ return {
       --   StatusLineNC = { bg = "none" },
       --   NeoTreeNormal = { bg = "none" },
       --   NeoTreeNormalNC = { bg = "none" },
-      -- },
-      -- init = { -- this table overrides highlights in all themes
-      --   Normal = { bg = "none" },
-      --   SignColumn = { bg = "none" },
-      --   FoldColumn = { bg = "none" },
-      --   NormalFloat = { bg = "none" },
-      --   NormalNC = { bg = "none" },
-      --   NormalSB = { bg = "none" },
-      --   FloatBorder = { bg = "none" },
-      --   FloatTitle = { fg = "#F2F3F5", bg = "none" },
-      --   -- WinBar
-      --   WinBar = { bg = "none" },
-      --   WinSeparator = { fg = "#F2F3F5", bg = "none" },
-      --   WinBarNC = { bg = "none" },
-      --   -- Diagnosis
-      --   DiagnosticVirtualTextHint = { fg = "#F2F3F5", bg = "none" },
-      --   DiagnosticVirtualTextWarn = { fg = "#e0af68", bg = "none" },
-      --   DiagnosticVirtualTextInfo = { fg = "#9ece6a", bg = "none" },
-      --   DiagnosticVirtualTextError = { fg = "#bd2c00", bg = "none" },
-      --   -- NeoTree
-      --   NeoTreeNormal = { bg = "none" },
-      --   NeoTreeNormalNC = { bg = "none" },
-      --   NeoTreeTabInactive = { bg = "none" },
-      --   NeoTreeTabSeperatorActive = { fg = "#F2F3F5", bg = "none" },
-      --   NeoTreeTabSeperatorInactive = { fg = "#F2F3F5", bg = "none" },
-      --   NvimTreeTabSeperatorActive = { fg = "#F2F3F5", bg = "none" },
-      --   NvimTreeTabSeperatorInactive = { fg = "#F2F3F5", bg = "none" },
-      --   MiniTabLineFill = { fg = "#F2F3F5", bg = "none" },
-      --   -- StatusLine
-      --   StatusLine = { fg = "#F2F3F5", bg = "none" },
-      --   StatusLineNC = { fg = "#F2F3F5", bg = "none" },
-      --   StatusLineTerm = { fg = "#F2F3F5", bg = "none" },
-      --   StatusLineTermNC = { fg = "#F2F3F5", bg = "none" },
-      --   VertSplit = { fg = "#F2F3F5", bg = "none" },
-      --   WhichKeyFloat = { bg = "none" },
-      --   -- Telescope
-      --   TelescopeBorder = { bg = "none" },
-      --   TelescopePromptTitle = { bg = "none" },
-      --   TelescopePromptBorder = { bg = "none" },
-      --   TelescopeNormal = { bg = "none" },
-      --   -- QuickFixLine
-      --   QuickFixLine = { bg = "none" },
-      --   -- TabLine
-      --   TabLine = { bg = "none" },
-      --   TabLineSel = { bg = "none" },
-      --   TabLineFill = { bg = "none" },
-      --   -- Cursor
-      --   CursorLineNr = { bg = "none" },
-      --   CursorLine = { bg = "none" },
-      --   -- CursorLine = { bg = "#262237" },
-      --   ColorColumn = { bg = "none" },
-      --   -- Search
-      --   -- Search = { fg = "red" },
-      --   -- IncSearch = { fg = "red" },
-      --   -- Pmenu
-      --   Pmenu = { bg = "none" },
-      --   PmenuSel = { bg = "none" },
-      --   PmenuSbar = { bg = "none" },
-      --   PmenuThumb = { bg = "none" },
-      --   -- Notifications
-      --   NotifyINFOBody = { bg = "none" },
-      --   NotifyWARNBody = { bg = "none" },
-      --   NotifyERRORBody = { bg = "none" },
-      --   NotifyDEBUGBody = { bg = "none" },
-      --   NotifyTRACEBody = { bg = "none" },
-      --   NotifyINFOBorder = { bg = "none" },
-      --   NotifyWARNBorder = { bg = "none" },
-      --   NotifyERRORBorder = { bg = "none" },
-      --   NotifyDEBUGBorder = { bg = "none" },
-      --   NotifyTRACEBorder = { bg = "none" },
-      --   NotifyBackground = { bg = "#000000" },
       -- },
     },
     -- Icons can be configured throughout the interface
